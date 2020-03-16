e4c5471ce626f25bc8b085640c776bf5fb7a239f
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2020-03-17 03:30:42 +0600

    Create calculator add & sub

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,15 +8,44 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText1,editText2;
+    private Button addButt,subButt;
+    private TextView result;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
+            editText1 = this.<EditText>findViewById(R.id.editeText1Id);
+            editText2 = this.<EditText>findViewById(R.id.editeText2Id);
+
+            addButt = this.<Button>findViewById(R.id.addButtId);
+            subButt = this.<Button>findViewById(R.id.subButtId);
+
+            result = this.<TextView>findViewById(R.id.resultId);
+
+        }
+        public void showMessage(View v){
+            String num1 = editText1.getText().toString();
+            String num2 = editText2.getText().toString();
+
+            //Converting int double
+            Double num_1 = Double.parseDouble(num1);
+            Double num_2 = Double.parseDouble(num2);
+
+            if(v.getId() == R.id.addButtId){
+                Double sum = num_1 + num_2;
+                result.setText("Result : " +sum);
+            }
+            else if(v.getId() == R.id.subButtId){
+                Double sum = num_1 - num_2;
+                result.setText("Result : " +sum);
+            }
         }
     }
