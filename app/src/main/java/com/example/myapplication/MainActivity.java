6d9561b293fe261cfce53d218a82266fb3d1dcbb
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2020-03-17 01:16:08 +0600

    Add Toast show

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,9 +30,15 @@ public class MainActivity extends AppCompatActivity {
     public void showMessage(View v){
         if(v.getId() == R.id.loginButtId){
 
+            Toast toast = Toast.makeText(MainActivity.this,"Login button is clicked",Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
         }
         else if(v.getId() == R.id.logoutButtId){
-           
+
+            Toast toast = Toast.makeText(MainActivity.this,"Logout button is clicked",Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
         }
     }
 }
