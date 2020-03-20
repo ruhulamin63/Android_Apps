f2215c8779ea95d3026ba8d275d4a2021d670b18
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2020-03-21 00:53:12 +0600

    edite string

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,50 +14,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText1,editText2;
-    private Button addButt,subButt;
-    private TextView result;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            editText1 = this.<EditText>findViewById(R.id.editeText1Id);
-            editText2 = this.<EditText>findViewById(R.id.editeText2Id);
 
-            addButt = this.<Button>findViewById(R.id.addButtId);
-            subButt = this.<Button>findViewById(R.id.subButtId);
-
-            result = this.<TextView>findViewById(R.id.resultId);
 
         }
         public void showMessage(View v){
 
             try
             {
-                String num1 = editText1.getText().toString();
-                String num2 = editText2.getText().toString();
 
-                //Converting int double
-                Double num_1 = Double.parseDouble(num1);
-                Double num_2 = Double.parseDouble(num2);
-
-                if(v.getId() == R.id.addButtId){
-                    Double sum = num_1 + num_2;
-                    result.setText("Result : " +sum);
-                }
-                else if(v.getId() == R.id.subButtId){
-                    Double sub = num_1 - num_2;
-                    result.setText("Result : " +sub);
-                }
-                else if(v.getId() == R.id.mulButtId){
-                    Double mul = num_1 * num_2;
-                    result.setText("Result : " +mul);
-                }
-                else if(v.getId() == R.id.divButtId){
-                    Double div = num_1 / num_2;
-                    result.setText("Result : " +div);
-                }
             }catch (Exception e){
                 Toast.makeText(MainActivity.this,"Please enter your number",Toast.LENGTH_SHORT).show();
             }
