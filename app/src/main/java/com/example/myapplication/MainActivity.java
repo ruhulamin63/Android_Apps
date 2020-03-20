5e3de28a461203898fd6afb4b94c9098983bd265
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2020-03-21 02:02:35 +0600

    Image view created

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -12,24 +13,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<imageView extends View> extends AppCompatActivity {
 
+    private imageView imageView1,imageView2;
 
+        @SuppressLint("WrongViewCast")
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-
-
+            imageView1 = this.<imageView> findViewById(R.id.imageView1Id);
+            imageView2 = this.<imageView>findViewById(R.id.imageView2Id);
         }
-        public void showMessage(View v){
-
-            try
-            {
-
-            }catch (Exception e){
-                Toast.makeText(MainActivity.this,"Please enter your number",Toast.LENGTH_SHORT).show();
+        public void showMessage(View v) {
+            if (v.getId() == R.id.imageView1Id) {
+                Toast.makeText(MainActivity.this, "Wow pic", Toast.LENGTH_SHORT).show();
+            } else if (v.getId() == R.id.imageView2Id) {
+                Toast.makeText(MainActivity.this, "Nice pic", Toast.LENGTH_SHORT).show();
             }
         }
     }
