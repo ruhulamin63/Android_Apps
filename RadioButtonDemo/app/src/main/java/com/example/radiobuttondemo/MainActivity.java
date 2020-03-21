76ab13f2fa772928ed86b11e6231253b66484423
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2020-03-22 01:40:48 +0600

    SeekBarDemo

--- RadioButtonDemo/app/src/main/java/com/example/radiobuttondemo/MainActivity.java
+++ RadioButtonDemo/app/src/main/java/com/example/radiobuttondemo/MainActivity.java
@@ -8,11 +8,14 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RatingBar ratingBar;
+    private SeekBar seekBar;
     private TextView resulTtextView;
 
     @Override
@@ -20,14 +23,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ratingBar = findViewById(R.id.RatingBarId);
+        seekBar = findViewById(R.id.SeekBarId);
         resulTtextView = findViewById(R.id.textViewId);
-            resulTtextView.setText("Value : "+ratingBar.getProgress());
 
-            ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        resulTtextView.setText("Volume : " + seekBar.getProgress() + "/" + seekBar.getMax());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                 @Override
-                public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                    resulTtextView.setText("Value : "+ratingBar.getProgress());
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    resulTtextView.setText("Volume : " + i + "/" + seekBar.getMax());
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    Toast.makeText(MainActivity.this,"onStartTrackingTouch",Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    Toast.makeText(MainActivity.this,"onStopTrackingTouch",Toast.LENGTH_SHORT).show();
                 }
             });
         }
