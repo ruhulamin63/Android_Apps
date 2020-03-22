1ac82d456091d395100751eb0188472efe4982dd
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2020-03-22 13:54:14 +0600

    Create by switcha-button

--- RadioButtonDemo/app/src/main/java/com/example/radiobuttondemo/MainActivity.java
+++ RadioButtonDemo/app/src/main/java/com/example/radiobuttondemo/MainActivity.java
@@ -5,44 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SeekBar seekBar;
-    private TextView resulTtextView;
+    private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        seekBar = findViewById(R.id.SeekBarId);
-        resulTtextView = findViewById(R.id.textViewId);
+            aSwitch = findViewById(R.id.SwitchId);
 
-        resulTtextView.setText("Volume : " + seekBar.getProgress() + "/" + seekBar.getMax());
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
-                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                    resulTtextView.setText("Volume : " + i + "/" + seekBar.getMax());
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-                    Toast.makeText(MainActivity.this,"onStartTrackingTouch",Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                    Toast.makeText(MainActivity.this,"onStopTrackingTouch",Toast.LENGTH_SHORT).show();
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(b){
+                        Toast.makeText(MainActivity.this,"On",Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(MainActivity.this,"Off",Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
+
         }
     }
