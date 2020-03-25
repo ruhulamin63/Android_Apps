5994171ab2a893b79d872de01dcf5c99b14b6fdd
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2020-03-25 16:46:54 +0600

    CreateIntentActivityDemo

---
+++ IntentMovingActivity/app/src/main/java/com/example/intentmovingactivity/FirstActivity.java
@@ -0,0 +1,25 @@
+package com.example.intentmovingactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = this.<Button>findViewById(R.id.buttonId);
+    }
+    public void onClickFirstActivity(View v){
+        Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+            startActivity(intent);
+    }
+}
